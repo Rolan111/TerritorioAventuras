d3b6453527abb0ef5dce1dc306d8ab522b735797
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2024-02-12 15:45:22 -0500

    Primera inclusión del video

--- Assets/Scripts/Mundo 1/DisparadorDeVideo.cs
+++ Assets/Scripts/Mundo 1/DisparadorDeVideo.cs
@@ -1,18 +1,58 @@
+using SUPERCharacter;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class DisparadorDeVideo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public VideoPlayer videoPlayer;
+    public GameObject personaje;
+    public GameObject activarDesactivarTexto;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            activarDesactivarTexto.SetActive(true);
+            ReproducirVideo();
+            Debug.Log("El objeto chocón con el jugador.");
+            
+            //Destroy(other.gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    private bool videoReproduciendo = false;
+
+    void Start()
     {
-        
+        videoPlayer.loopPointReached += VideoTerminado;
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space) && !videoReproduciendo)
+    //    {
+    //        ReproducirVideo();
+    //    }
+    //}
+
+    void ReproducirVideo()
+    {
+        videoReproduciendo = true;
+        personaje.GetComponent<SUPERCharacterAIO>().enabled = false; // Llama a un método en el script de movimiento del personaje para pausar su movimiento
+        videoPlayer.Play();
+    }
+
+    void VideoTerminado(VideoPlayer vp)
+    {
+        activarDesactivarTexto.SetActive(false);
+        videoReproduciendo = false;
+        Destroy(gameObject);
+        Destroy(videoPlayer);
+        personaje.GetComponent<SUPERCharacterAIO>().enabled = true; // Llama a un método en el script de movimiento del personaje para reanudar su movimiento
     }
 }
