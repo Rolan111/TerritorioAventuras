ef31b327d06a5ca3f775a3a7ae3e6d14bc081364
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2024-02-22 11:17:44 -0500

    Corrección minijuego 2 y reacomodación de los portales

--- Assets/Scripts/WinControllerScript.cs
+++ Assets/Scripts/WinControllerScript.cs
@@ -1,36 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class WinControllerScript : MonoBehaviour
-{
-    public GameObject planets;
+//public class WinControllerScript : MonoBehaviour
+//{
+//    public GameObject planets;
 
-    private int pointsToWin;
-    private int currentPoints;
+//    private int pointsToWin;
+//    private int currentPoints;
 
-    void Start()
-    {
-        pointsToWin = planets.transform.childCount;
-    }
+//    void Start()
+//    {
+//        pointsToWin = planets.transform.childCount;
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (currentPoints >= pointsToWin)
-        {
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (currentPoints >= pointsToWin)
+//        {
 
-        }
-    }
+//        }
+//    }
 
 
-    public void AddPoints()
-    {
-        currentPoints++;
-    }
+//    public void AddPoints()
+//    {
+//        currentPoints++;
+//    }
 
-    public bool ValidateWin() => currentPoints >= pointsToWin;
+//    public bool ValidateWin() => currentPoints >= pointsToWin;
 
-    public int GetCurrentPoints() => currentPoints;
-    public int GetPointsToWin() => pointsToWin;
-}
+//    public int GetCurrentPoints() => currentPoints;
+//    public int GetPointsToWin() => pointsToWin;
+//}
