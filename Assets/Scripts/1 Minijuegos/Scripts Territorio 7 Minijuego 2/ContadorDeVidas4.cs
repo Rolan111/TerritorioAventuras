acc88c2fd29e763e22443b5977884b1e15b0de25
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2024-05-06 22:39:09 -0500

    Ajustes en la resolución de los minijuegos

--- Assets/Scripts/1 Minijuegos/Scripts Territorio 7 Minijuego 2/ContadorDeVidas4.cs
+++ Assets/Scripts/1 Minijuegos/Scripts Territorio 7 Minijuego 2/ContadorDeVidas4.cs
@@ -1,9 +1,5 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
