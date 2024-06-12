ca47841bd71497356d6decacdf3af0842a0ceb39
Rolan111 <97992969+Rolan111@users.noreply.github.com>
2024-06-12 12:04:09 -0500

    Integración final del tablero de control y selección individual de mundos

--- Assets/Scripts/Mundo 1/ScriptNoDestruir.cs
+++ Assets/Scripts/Mundo 1/ScriptNoDestruir.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+using UnityEngine.SceneManagement;
 
-public class ScriptNoDestruir : MonoBehaviour
+public class ScriptNoDestruir : MonoBehaviour //No destruye y a la vez se asegura que haya una
+                                              //sola instancia del objeto
 {
-    // Start is called before the first frame update
+    private static ScriptNoDestruir instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Al cambiar de escena, verifica si "Cubo" ya existe.
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject existingCubo = GameObject.Find("CanvasTableroControl");
+        if (existingCubo != null && existingCubo != this.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 }
